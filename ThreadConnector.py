e4d31e6506246478d345e9bc1dd024e35d38661c
r1317 <122949375+r-1317@users.noreply.github.com>
2023-12-24 17:51:07 +0900

    update 1 file and create 1 file

--- ThreadConnector.py
+++ ThreadConnector.py
@@ -1,3 +1,10 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="電子掲示板の複数partに別れたスレッドを結合します。")
+
+# 引数の設定
+parser.add_argument("latest_url", help = "最新のスレッドのURL")
+parser.add_argument("part_number", type = int, help = "最新スレッドのpart数")
+parser.add_argument("")
+
+args = parser.parse_args()
